2349437df43c8324233251e2ccc117ed626f0e7e
rianran1993 <rianran1993@163.com>
2018-06-08 17:40:08 +0800

    cordove init

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,35 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/jpush-phonegap-plugin/www/JPushPlugin.js",
+        "id": "jpush-phonegap-plugin.JPushPlugin",
+        "clobbers": [
+            "JPush"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.3",
+    "cordova-plugin-splashscreen": "3.1.0",
+    "cordova-plugin-device": "2.0.2",
+    "cordova-plugin-jcore": "1.2.0",
+    "jpush-phonegap-plugin": "3.4.1"
+};
+// BOTTOM OF METADATA
+});
